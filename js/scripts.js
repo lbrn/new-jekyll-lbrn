8ffc1810c9f82cb49e9cfad97ad5d8df507fcaa8
mjtheriot4 <mther16@lsu.edu>
2017-07-06 21:53:40 -0500

    fixed menu bug

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,15 @@ $( document ).ready(function() {
 //Mobile Menu
   $("#navBug").click(function(){
       $('nav').slideToggle('fast');
-      if ($(this).attr("src") == "/images/menuOpen.png") {
-        $(this).attr("src", "/images/menuClosed.png");
+      if ($(this).attr("src") == "images/menuOpen.png") {
+        $(this).attr("src", "images/menuClosed.png");
       }
-      else {$(this).attr("src", "/images/menuOpen.png")}
+      else {$(this).attr("src", "images/menuOpen.png")}
   });
   $("body > div.layout").click(function(){
     if ($('#navBug').css('display') != 'none'){
       $('nav').slideUp('fast');
-      $('#navBug').attr("src", "/images/menuClosed.png")
+      $('#navBug').attr("src", "images/menuClosed.png")
     }
   });
   $(window).resize(function(){
